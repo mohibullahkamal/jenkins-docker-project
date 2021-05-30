4474a28f4b3ec91e62adc34033f3c7e4b15fe52a
mogit2020 <mohibullah.kamal@gmail.com>
2021-05-31 00:41:09 +0200

    add changes

--- automate.sh
+++ automate.sh
@@ -1,23 +1,25 @@
 #! /bin/bash
 
-read -p "Enter how many times---> " COUNTER
-if ! [[ "$scale" =~ ^[0-9]+$ ]]
-    then
-        echo "Sorry integers only"
-fi
+# read -p "Enter how many times---> " NUM
 
-until [ $COUNTER -le 0 ]; do
-   echo TheCounterIs----> $COUNTER
+# if ! [[ "$NUM" =~ ^[0-9]+$ ]]
+#    then
+#       echo "Sorry integers only"
+# fi 
+
+# COUNTER=$NUM
 
 
+COUNTER=1
+while [ $COUNTER -le 0 ]
+do
+   echo "TheCounterIs---->  $COUNTER"
    if [ -e "1.txt" ] 
    then
       rm     1.txt
    else
       touch  1.txt 
    fi
-
-	
    git add .
    git commit -m 'minor commit'
    git push origin main
