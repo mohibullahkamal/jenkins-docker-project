dd8ecff193c8b43ad8db90714ebad26584d938fb
mogit2020 <mohibullah.kamal@gmail.com>
2021-05-30 21:06:25 +0200

    edit .sh file

--- myScript.sh
+++ myScript.sh
@@ -7,3 +7,16 @@ mkdir hello
 touch "hello/world.txt"
 echo "Hello World" >> "hello/world.txt" 
 echo "Created hello/world.txt"
+
+# CREATE .TXT FILES
+touch 1.txt 2.txt 3.txt
+
+# FOR LOOP TO RENAME FILES
+FILES=$(ls *.txt)
+NEW="new"
+for FILE in $FILES  
+  do
+    echo "Renaming $FILE to new-$FILE"
+    mv $FILE $NEW-$FILE
+done
+
