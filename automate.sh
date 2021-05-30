3e6759548560c78164055ad2aa8bac9222a4a40d
mogit2020 <mohibullah.kamal@gmail.com>
2021-05-31 01:00:50 +0200

    minor commit

--- automate.sh
+++ automate.sh
@@ -5,23 +5,24 @@ read -p "Enter how many times---> " NUM
 if ! [[ "$NUM" =~ ^[0-9]+$ ]]
    then
       echo "Sorry integers only"
-fi 
 
-COUNTER=$NUM
-zero=0
-
-until [ "$COUNTER" -le "$zero" ]
-do
-   echo "TheCounterIs---->  $COUNTER"
-   if [ -e "1.txt" ] 
-   then
-      rm     1.txt
    else
-      touch  1.txt 
-   fi
-   git add .
-   git commit -m 'minor commit'
-   git push origin main
+      COUNTER=$NUM
+      zero=0
+      until [ "$COUNTER" -le "$zero" ]
+      do
+         echo "TheCounterIs---->  $COUNTER"
+         if [ -e "1.txt" ] 
+         then
+            rm     1.txt
+         else
+            touch  1.txt 
+         fi
+         git add .
+         git commit -m 'minor commit'
+         git push origin main
 
-   let COUNTER-=1
-done
+         let COUNTER-=1
+      done
+
+fi
