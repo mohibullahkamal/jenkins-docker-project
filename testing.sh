8dc274c6f050c762a75a8479fa1284f2237b3092
mogit2020 <mohibullah.kamal@gmail.com>
2021-06-01 20:39:12 +0200

    minor commit

---
+++ testing.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+
+
+
+
+
+
+
+
+
+
+
+
+# read -p "Enter how many times---> " COUNTER
+
+# if ! [[ "$COUNTER" =~ ^[0-9]+$ ]]
+#    then
+#       echo "Sorry integers only"
+
+#    else
+#       until [ "$COUNTER" -le 0 ]
+#       do
+#          echo "TheCounterIs---->  $COUNTER"
+#          if [ -e "1.txt" ] 
+#          then
+#             rm     1.txt
+#          else
+#             touch  1.txt 
+#          fi
+#          git add .
+#          git commit -m 'minor commit'
+#          git push origin main
+
+#          let COUNTER-=1
+#       done
+
+# fi
